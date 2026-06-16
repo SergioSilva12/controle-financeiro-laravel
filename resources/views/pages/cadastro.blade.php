4cc0a1a2b4fc0b364acd6a6c27ebaf6ec8db3cca
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-06-16 18:25:42 -0300

    Criando o model de usuarios

--- resources/views/pages/cadastro.blade.php
+++ resources/views/pages/cadastro.blade.php
@@ -1,18 +1,24 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <form method="post" action="#">
         @csrf
-        <input type="text">
-        <input type="text">
-        <input type="text">
-        <input type="text">
+        <input type="text" name="nome" id="nome" placeholder="Digite o seu nome">
+
+        <input type="text" name="email" id="email" name="email" placeholder="Digite o seu email">
+
+        <input type="password" id="senha" name="senha" placeholder="Digite a sua senha">
+
+        <input type="radio">
     </form>
 </body>
+
 </html>
