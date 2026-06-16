9b9f487ac0b9633856bc59e30c0e400a0f39228e
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-06-16 18:55:44 -0300

    Criando as regras e feedbacks do cadastro de usuarios (OK)

--- app/Http/Controllers/ControllerUsuario.php
+++ app/Http/Controllers/ControllerUsuario.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 use App\Model\Usuario;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class ControllerUsuario extends Controller
 {
@@ -17,28 +18,30 @@ class ControllerUsuario extends Controller
             'nome' => 'required|string|max:255',
             'email' => 'required|email|max:150|unique:usuarios,email',
             'senha' => 'required|min:6|string|confirmed',
-            'aceite_termos'=> 'required,accepted'
+            'aceite_termos' => 'required,accepted'
         ];
 
         $feedback = [
-            'nome.required'=>'O nome é obrigatório.',
-            'email.required'=> 'O E-mail é obrigatório.',
-            'email.email'=> 'Informe um E-mail válido.',
-            'email.unique'=>'Este E-mail já esta cadastrado.',
-            'senha.required'=>'A senha é obrigatório.',
-            'senha.min'=>'A senha deve ter no mínimo 6 caracteres.',
-            'aceite_termos.required'=> 'Você precisa aceitar os termos de uso.'
+            'nome.required' => 'O nome é obrigatório.',
+            'email.required' => 'O E-mail é obrigatório.',
+            'email.email' => 'Informe um E-mail válido.',
+            'email.unique' => 'Este E-mail já esta cadastrado.',
+            'senha.required' => 'A senha é obrigatório.',
+            'senha.min' => 'A senha deve ter no mínimo 6 caracteres.',
+            'aceite_termos.required' => 'Você precisa aceitar os termos de uso.'
         ];
-    
-        Usuario::creeate([
-            'nome'=>$request->nome,
-            'email'=>$request->email,
-            'senha'=>Hash::make($request->senha),
+
+        $request->validate($regras, $feedback);
+
+        Usuario::create([
+            'nome' => $request->nome,
+            'email' => $request->email,
+            'senha' => Hash::make($request->senha),
 
         ]);
-        
-        return redirect()->route('cadastro.cadastrar')->with('success','Usuario cadastrado com sucesso');
-    
-        
+
+        return redirect()->route('cadastro.cadastrar')->with('success', 'Usuario cadastrado com sucesso');
+
+
     }
 }
