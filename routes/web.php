7ae2b72185819b9b957bbfd3091558d1dcff842c
Sérgio Silva <sergiolimasilva12@gmail.com>
2026-06-16 18:09:46 -0300

    Iniciando a criação das rotas

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.home');
 });
+
+Route::get('/cadastro', function () {
+    return view('pages.cadastro');
+});
+Route::post('/cadastro', function () {
+    return view('pages.cadastro');
+});
+
